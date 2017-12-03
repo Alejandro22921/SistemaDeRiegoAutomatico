632b68cfb85f96682e27b1ac3e49ba3967792079
Alejandro22921 <alejandro.hdz@ieee.org>
2017-12-02 21:43:32 -0600

    a
    a

--- DBRiegoAutomatizado.cs
+++ DBRiegoAutomatizado.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SistemaDeRiegoAutomatico
 {
@@ -18,6 +19,55 @@ namespace SistemaDeRiegoAutomatico
             conexion = new SqlConnection("Data Source=LAPTOP-7JPH5U2H\\SQLExpress;Initial Catalog=DBRiegoAutomatizado;Integrated Security=True");
             conexion.Open();
         }
+
+        public static void listaProgramadoRiego()
+        {
+            string consulta = "SELECT Id, HActivar, HDesactivar FROM ProgramadoRiego";
+            SqlCommand comando = new SqlCommand(consulta, conexion);
+            SqlDataReader reader = comando.ExecuteReader();
+            while (reader.Read())
+            {
+                MainWindow.listaProgramadoRiego.Add(new ConfiguracionProgramado(reader.GetInt32(0), reader.GetString(1), reader.GetString(1)));
+            }
+            reader.Close();
+        }
+
+        public static void listaProgramadoIluminacion()
+        {
+            string consulta = "SELECT Id, HActivar, HDesactivar FROM ProgramadoIluminacion";
+            SqlCommand comando = new SqlCommand(consulta, conexion);
+            SqlDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                MessageBox.Show(reader.GetString(1));
+                MainWindow.listaProgramadoIluminacion.Add(new ConfiguracionProgramado(reader.GetInt32(0), reader.GetString(1), reader.GetString(1)));
+            }
+            reader.Close();
+        }
+
+        public static void GuardarConfiguracionRiego()
+        {
+            //string query = string.Format("DELETE FROM ProgramadoRiego");
+            //SqlCommand comando1 = new SqlCommand(query, conexion);
+
+            int cont = 0;
+            foreach (ConfiguracionProgramado aux in MainWindow.listaProgramadoRiego)
+            {
+                string query = string.Format("INSERT INTO ProgramadoRiego (HActivar, HDesactivar) VALUES ('{0}', '{1}')", aux.HActivar, aux.HDesactivar);
+                SqlCommand comando3 = new SqlCommand(query, conexion);
+                MessageBox.Show(query);
+                cont++;
+            }
+        }
+
+        public static void GuardarConfiguracionIluminacion()
+        {
+            string query = string.Format("DELETE FROM ProgramadoIluminacion");
+            SqlCommand comando2 = new SqlCommand(query, conexion);
+        }
+
+        
 
     }
 }
--- wProgramado.xaml.cs
+++ wProgramado.xaml.cs
@@ -19,9 +19,198 @@ namespace SistemaDeRiegoAutomatico
     /// </summary>
     public partial class wProgramado : Window
     {
+        private List<ConfiguracionProgramado> listaProgramadoRiegoAux = new List<ConfiguracionProgramado>();
+        private List<ConfiguracionProgramado> listaProgramadoIluminacionAux = new List<ConfiguracionProgramado>();
+
         public wProgramado()
         {
             InitializeComponent();
+            listViewProgramado.ItemsSource = MainWindow.listaProgramadoRiego;
+            listViewProgramado.Items.Refresh();
+            listaProgramadoRiegoAux = MainWindow.listaProgramadoRiego;
+            listaProgramadoIluminacionAux = MainWindow.listaProgramadoIluminacion;
+        }
+
+        private void txtHoras_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt16(txtHorasR_A.Text) > 23)
+                    txtHorasR_A.Text = "23";
+            }
+            catch (Exception) { }
+        }
+
+        private void txtSegundos_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt16(txtSegundosR_A.Text) > 59)
+                    txtSegundosR_A.Text = "59";
+            }
+            catch (Exception) { }
+        }
+
+        private void txtMinutosR_A_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt16(txtMinutosR_A.Text) > 59)
+                    txtMinutosR_A.Text = "59";
+            }
+            catch (Exception) { }
+        }
+
+
+        private void txtSegundosR_D_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt16(txtSegundosR_D.Text) > 59)
+                    txtSegundosR_D.Text = "59";
+            }
+            catch (Exception) { }
+        }
+
+        private void txtMinutosR_D_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt16(txtMinutosR_D.Text) > 59)
+                    txtMinutosR_D.Text = "59";
+            }
+            catch (Exception) { }
+        }
+
+        private void txtHorasR_D_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt16(txtHorasR_D.Text) > 23)
+                    txtHorasR_D.Text = "23";
+            }
+            catch (Exception) { }
+        }
+
+        private void txtHorasR_A_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+
+            else
+                e.Handled = true;
+        }
+
+        private void txtSegundosR_A_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+
+            else
+                e.Handled = true;
+        }
+
+        private void txtMinutosR_A_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+
+            else
+                e.Handled = true;
+        }
+
+        private void txtHorasR_D_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+
+            else
+                e.Handled = true;
+        }
+
+        private void txtMinutosR_D_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+
+            else
+                e.Handled = true;
+        }
+
+        private void txtSegundosR_D_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+
+            else
+                e.Handled = true;
+        }
+
+        private void btnAñadirRiego_Click(object sender, RoutedEventArgs e)
+        {
+            listaProgramadoRiegoAux.Add(new ConfiguracionProgramado(0, txtHorasR_A.Text + ":" + txtMinutosR_A.Text + ":" + txtSegundosR_A.Text, txtHorasR_D.Text + ":" + txtMinutosR_D.Text + ":" + txtSegundosR_D.Text));
+            listViewProgramado.ItemsSource = listaProgramadoRiegoAux;
+            listViewProgramado.Items.Refresh();
+        }
+
+        private void txtHorasR_A_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtHorasR_A.Text = verificarHora2(txtHorasR_A.Text);
+        }
+
+        private void txtMinutosR_A_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtMinutosR_A.Text = verificarHora2(txtMinutosR_A.Text);
+        }
+
+        private void txtSegundosR_A_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtSegundosR_A.Text = verificarHora2(txtSegundosR_A.Text);
+        }
+
+        private void txtHorasR_D_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtHorasR_D.Text = verificarHora2(txtHorasR_D.Text);
+        }
+
+        private void txtMinutosR_D_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtMinutosR_D.Text = verificarHora2(txtMinutosR_D.Text);
+        }
+
+        private void txtSegundosR_D_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtSegundosR_D.Text = verificarHora2(txtSegundosR_D.Text);
+        }
+
+        // SE ELIMINAN LOS ELEMENTOS DE LAS LISTAS.
+        private void btnEliminarRiego_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (listViewProgramado.SelectedItems.Count == 1)
+                {
+                    listaProgramadoRiegoAux.RemoveAt(listViewProgramado.SelectedIndex);
+                    listViewProgramado.ItemsSource = listaProgramadoRiegoAux;
+                    listViewProgramado.Items.Refresh();
+                }
+            }catch(Exception){}
+        }
+
+        private string verificarHora2(string x)
+        {
+            if (x == "")
+                return "00";
+            if (Convert.ToInt16(x) < 10 && x.Length == 1)
+                return "0" + x;
+            return x;
+        }
+
+        private void btnGuardarConfiguracion_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.listaProgramadoRiego = listaProgramadoRiegoAux;
+            MainWindow.listaProgramadoIluminacion = listaProgramadoIluminacionAux;
+            DBRiegoAutomatizado.GuardarConfiguracionRiego();
         }
     }
 }
